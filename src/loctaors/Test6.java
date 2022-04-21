14a193808ca3860a660cc482bc9163f3732fe1d7
QSP <QSP@QSP>
2022-04-21 09:18:59 +0530

    Locators

---
+++ src/loctaors/Test6.java
@@ -0,0 +1,21 @@
+package loctaors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoapp.skillrary.com/login.php?type=login");
+	driver.findElement(By.cssSelector("input[id='email']")).sendKeys("admin");
+	driver.findElement(By.cssSelector("input[id='password']")).sendKeys("manager");
+	driver.findElement(By.cssSelector("button[class='login_Btn']")).click();
+	
+	
+	}
+
+}
